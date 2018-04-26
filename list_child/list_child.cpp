3246aaae72d4cd2f34e07b412ad584cb7a66079c
Revaldi Gilang Azhari <32513540+OMValdi@users.noreply.github.com>
2018-04-26 21:18:01 +0700

    Update list_child.cpp

--- list_child/list_child.cpp
+++ list_child/list_child.cpp
@@ -19,13 +19,17 @@ address_child alokasi(infotype_child x){
 void insertFirst(List_child &L, address_child P){
     if (first(L) == NULL){
         first(L) = P;
-        prev(P) = first(L);
+        last(L) = P;
+        next(P) = P;
+        prev(P) = P;
 
 
     } else{
+        next(last(L)) = P;
+        prev(P) = last(L);
         next(P) = first(L);
         prev(first(L)) = P;
-        first(L) = P;
+        last(L) = P;
 
     }
 }
@@ -36,37 +40,71 @@ void insertLast(List_child &L, address_child P){
     } else{
         next(last(L))= P;
         prev(P) = last(L);
+        next(P) = first(L);
+        prev(first(L)) = P;
         last(L) = P;
     }
 
 
 }
 void deleteFirst(List_child &L, address_child &P){
-        if(first(L) ==  NULL){
-         insertFirst(L,P);
-     } else{
-        P = first(L);
-        first(L)= next(P);
-        next(P)  = NULL;
-        prev(first(L)) = NULL;
+        if(first(L) !=  NULL){
+         P =first(L);
+         if(P != last(L)){
+            next(P) = NULL;
+            prev(P) = NULL;
+            first(L) = NULL;
+            last(L) = NULL;
+            }else{
+            first(L) = prev(P);
+            next(last(L)) = first(L);
+            prev(first(L)) = last(L);
+            next(P) = NULL;
+            prev(P) = NULL;
+
+           }
      }
 }
 void deleteLast(List_child &L, address_child &P){
-    if(first(L) == last(L)){
+        if (first(L) != NULL){
+            P = last(L);
+            if(P == last(L)){
+                next(P) = NULL;
+                prev(P) = NULL;
+                first(L) = NULL;
+                last(L) = NULL;
+            }else{
+                last(L) = prev(P);
+                next(last(L)) = first(L);
+                prev(first(L)) = last(L);
+                next(P)= NULL;
+                prev(P) = NULL;
+
+            }
+        }
+}
+void deleteAfter(List_child &L,address_child Prec, address_child &P){
+    if((first(L) != NULL) && (Prec != NULL)){
+       if(Prec != last(L)){
+            if(next(Prec)== last(L)){
+                deleteLast(L, P);
+            }else{
+                P = next(Prec);
+                next(Prec) = next(P);
+                prev(next(P))=  Prec;
+                next(P) = NULL;
+                prev(P) = NULL;
+       }
+       }else{
         deleteFirst(L,P);
-    }else{
-        P = last(L);
-        next(prev(P)) = NULL;
-        prev(P) = NULL;
-    }
-}
-void deleteAfter(address_child Prec, address_child &P){
+       }
+
+       }
 
 }
 
-void dealokasi(address_child &P){
 
-}
+
 address_child findElm(List_child L, infotype_child x){
     address_child P = first(L);
     while (P != NULL){
